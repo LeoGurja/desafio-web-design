7f39c44989778b054f37e893ce1485ac4aa1a847
joaovitor2614 <joaovitoralestrella@gmail.com>
2021-07-12 19:49:36 -0300

    remover glider.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import '../styles/index.css'
-import 'glider-js'
+
 import '@fortawesome/fontawesome-free/js/all'
 import '@fortawesome/fontawesome-free/css/fontawesome.css'
 
@@ -25,44 +25,3 @@ window.onclick = function(event) {
 }
 
 
-var $simpleCarousel = document.querySelector(".js-carousel--simple");
- 
-new Glider($simpleCarousel, {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: true,
-  itemWidth: 'auto',
-  dots: ".js-carousel--simple-dots",
-  arrows: {
-    prev: ".js-carousel--simple-prev",
-    next: ".js-carousel--simple-next",
-  },
-});
-
-new Glider($responsiveCarousel, {
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  itemWidth: 'auto',
-  draggable: true,
-  dots: ".js-carousel--responsive-dots",
-  arrows: {
-    prev: ".js-carousel--responsive-prev",
-    next: ".js-carousel--responsive-next",
-  },
-  responsive: [
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      },
-    },
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-      },
-    },
-  ],
-});
