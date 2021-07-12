d265d6cac561f2307688c590eab028d3f3cea43b
joaovitor2614 <joaovitoralestrella@gmail.com>
2021-07-12 19:39:26 -0300

    posicionar produtos

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import '../styles/index.css'
+import 'glider-js'
 import '@fortawesome/fontawesome-free/js/all'
 import '@fortawesome/fontawesome-free/css/fontawesome.css'
+
 // pegar elementos do modal
 const modal = document.getElementById('app-modal');
 const modalBtn = document.getElementById('modal-btn');
@@ -21,3 +23,46 @@ window.onclick = function(event) {
     modal.style.display = 'none'
   }
 }
+
+
+var $simpleCarousel = document.querySelector(".js-carousel--simple");
+ 
+new Glider($simpleCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  draggable: true,
+  itemWidth: 'auto',
+  dots: ".js-carousel--simple-dots",
+  arrows: {
+    prev: ".js-carousel--simple-prev",
+    next: ".js-carousel--simple-next",
+  },
+});
+
+new Glider($responsiveCarousel, {
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  itemWidth: 'auto',
+  draggable: true,
+  dots: ".js-carousel--responsive-dots",
+  arrows: {
+    prev: ".js-carousel--responsive-prev",
+    next: ".js-carousel--responsive-next",
+  },
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+      },
+    },
+  ],
+});
